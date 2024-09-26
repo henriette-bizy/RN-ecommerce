07a5de54a1ae7aed49a2fca0fcc8715a336ed8e8
henriette-bizy <hopebiziyaremye@gmail.com>
2024-09-26 07:49:16 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Routes from "./routes/Routes";
+import { Provider } from "react-redux";
+import { store } from "./states/store";
+
+export default function App() {
+  console.reportErrorsAsExceptions = false;
+  return (
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  );
+}
